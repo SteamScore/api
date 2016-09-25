b21007e2e30c6a3ae5cb2243cd14d315686d8293
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-09-25 15:11:08 +0200

    Add Zend Expressive

---
+++ config/autoload/routes.global.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'dependencies' => [
+        'invokables' => [
+            Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\ZendRouter::class,
+        ],
+        'factories' => [
+        ],
+    ],
+    'routes' => [
+        // Example:
+        // [
+        //     'name' => 'home',
+        //     'path' => '/',
+        //     'middleware' => App\Action\HomePageAction::class,
+        //     'allowed_methods' => ['GET'],
+        // ],
+    ],
+];
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+use Interop\Container\ContainerInterface;
+use Zend\Expressive\Application;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+/** @var ContainerInterface $container */
+$container = require 'config/container.php';
+
+/** @var Application $app */
+$app = $container->get(Application::class);
+
+$app->run();
