5b420e003efdea8fadeff67292b4a58cb6614010
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-09-25 20:24:33 +0200

    Change clover location

--- .travis.coverage.sh
+++ .travis.coverage.sh
@@ -2,5 +2,5 @@ set -x
 if [ "$TRAVIS_PHP_VERSION" = '7.0' ] ; then
     wget https://scrutinizer-ci.com/ocular.phar
     phpdbg -qrr ./vendor/bin/phpunit || :
-    php ocular.phar code-coverage:upload --format=php-clover ./clover.xml
+    php ocular.phar code-coverage:upload --format=php-clover ./build/logs/clover.xml
 fi
