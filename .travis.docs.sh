746890c02f3511d299bdada532f1951a30d1e018
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-09-25 22:20:45 +0200

    Don't generate docs

--- .travis.docs.sh
+++
@@ -1,5 +0,0 @@
-set -x
-if [ "$TRAVIS_PHP_VERSION" = '7.0' ] ; then
-    node_modules/.bin/hercule docs/steamscore.v1.apib -o build/steamscore.v1.apib
-    node_modules/.bin/aglio -i build/steamscore.v1.apib -o build/steamscore.v1.html --theme-full-width
-fi
