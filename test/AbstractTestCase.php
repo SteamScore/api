b4e742463f3b1f2202c2311636b55ea4ce7373ee
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-10-01 23:51:14 +0200

    Add test for VersionActionFactory

---
+++ test/AbstractTestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SteamScore.
+ *
+ * (c) SteamScore <code@steamscore.info>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace SteamScore\Api\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophet;
+
+abstract class AbstractTestCase extends TestCase
+{
+    /**
+     * @var Prophet
+     */
+    protected $prophet;
+
+    protected function setup()
+    {
+        $this->prophet = new Prophet();
+    }
+
+    protected function tearDown()
+    {
+        $this->prophet->checkPredictions();
+    }
+}
---
+++ test/Http/Factories/VersionActionFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SteamScore.
+ *
+ * (c) SteamScore <code@steamscore.info>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace SteamScore\Api\Tests\Http\Factories;
+
+use Interop\Container\ContainerInterface;
+use SteamScore\Api\Http\Actions\VersionAction;
+use SteamScore\Api\Http\Factories\VersionActionFactory;
+use SteamScore\Api\Tests\AbstractTestCase;
+
+final class VersionActionFactoryTest extends AbstractTestCase
+{
+    /**
+     * Tests that the factory is invokable.
+     */
+    public function testInvokability()
+    {
+        $container = $this->prophesize(ContainerInterface::class);
+        $factory = new VersionActionFactory();
+
+        $container->get('config')->willReturn(['version' => '1.0.0']);
+
+        $instance = $factory($container->reveal());
+
+        $this->assertInstanceOf(VersionAction::class, $instance);
+    }
+}
