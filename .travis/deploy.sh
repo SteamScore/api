704760fb374032c34973b9c2e17bc3c1f1ec25db
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-10-25 14:35:40 +0200

    Generate new SSH key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
-pip install ansible
-openssl aes-256-cbc -K $encrypted_a837f529038e_key -iv $encrypted_a837f529038e_iv -in ./.travis/id_ed25519.enc -out ./.travis/id_ed25519 -d
+pip install --user ansible
+openssl aes-256-cbc -K $encrypted_2fbddccfbf30_key -iv $encrypted_2fbddccfbf30_iv -in ./.travis/id_ed25519.enc -out ./.travis/id_ed25519 -d
 eval "$(ssh-agent -s)"
 chmod 600 ./.travis/id_ed25519
 ssh-add ./.travis/id_ed25519
