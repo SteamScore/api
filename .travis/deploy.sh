89572dc997007ee9f52736f2fc1d978494292d2d
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-10-25 15:06:32 +0200

    Decrypt SSH key before install

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 set -x
 pip install --user ansible
-openssl aes-256-cbc -K $encrypted_2fbddccfbf30_key -iv $encrypted_2fbddccfbf30_iv -in ./.travis/id_ed25519.enc -out ./.travis/id_ed25519 -d
-eval "$(ssh-agent -s)"
-chmod 600 ./.travis/id_ed25519
-ssh-add ./.travis/id_ed25519
