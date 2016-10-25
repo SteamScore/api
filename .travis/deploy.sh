9378480eb78360830a8f5472086d152a745d53aa
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-10-25 15:39:46 +0200

    Run ansible on deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 set -x
+openssl aes-256-cbc -K $encrypted_2fbddccfbf30_key -iv $encrypted_2fbddccfbf30_iv -in ./.travis/id_ed25519.enc -out ./.travis/id_ed25519 -d
+cp ./.travis/id_ed25519 $HOME/.ssh/id_ed25519
+chmod 600 $HOME/.ssh/id_ed25519
 pip install --user ansible
+$HOME/.local/bin/ansible-playbook ../.ansible/playbook.yml -i ../.ansible/hosts
