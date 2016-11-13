f72232af84928dc2e306c1d5c1dd39a43e5d6c8e
Jonas Stendahl <jonas.stendahl@gmail.com>
2016-11-13 13:32:45 +0100

    Fix ext-ast.sh

--- .travis/ext-ast.sh
+++ .travis/ext-ast.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-function build {
-    phpize
-    ./configure
-    make
-}
-
-function cleanBuild {
-    make clean
-    build
-}
-
-function install {
-    make install
-    echo "extension=ast.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-}
-
-if [[ -d "build" ]] ; then
-    mkdir build
-fi
+mkdir build
 
 git clone --depth 1 https://github.com/nikic/php-ast.git build/ast
 
